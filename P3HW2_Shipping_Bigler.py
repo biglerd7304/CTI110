604e5174a1b135ba1232440b60f620295fb2f479
biglerd7304 <43176266+biglerd7304@users.noreply.github.com>
2018-09-27 10:13:12 -0400

    Add files via upload

---
+++ P3HW2_Shipping_Bigler.py
@@ -0,0 +1,18 @@
+# CTI-110 
+# P3HW2 - Shipping Charges 
+# Daniel Bigler 
+# 9-27-18
+#
+def main():
+    # input
+    Weight = int(input('Enter the weight of the package to see the shipping carges: '))
+    # determine the range for each price
+    if Weight <= 2:
+        print('The shipping charge is $1.50')
+    elif Weight <= 6:
+        print('The shipping charge is $3.00')
+    elif Weight <= 10:
+        print('The shipping charge is 4.00')
+    else:
+        print('The shipping charge is 4.75')
+main()
